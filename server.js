19195b877af60f03c7d92abf79321e2418d29903
etienne <etiennerodriguez3@gmail.com>
2022-08-04 11:56:03 -0400

    able to view election table

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,23 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your sql username,
+        user: 'root',
+        // mysql password
+        password: 'Villa100!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
